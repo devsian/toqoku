a096d465b936250ea97af99eaf417045422e68ad
Muhammad Fakhrian Ikhwaludin <mdrianid@gmail.com>
2022-06-26 22:10:17 +0700

    Upload projecet Vue JS
    Slicing Front-End with Vue JS

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShaynaVue />
+    <HeroShaynaVue />
+    <WomanShaynaVue />
+    <InstaShaynaVue />
+    <PartnerShaynaVue />
+    <FooterShaynaVue />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShaynaVue from '@/components/HeaderShayna.vue'
+import HeroShaynaVue from '@/components/HeroShayna.vue'
+import WomanShaynaVue from '@/components/WomanShayna'
+import InstaShaynaVue from '@/components/InstaShayna'
+import PartnerShaynaVue from '@/components/PartnerShayna'
+import FooterShaynaVue from '@/components/FooterShayna'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShaynaVue,
+    HeroShaynaVue,
+    WomanShaynaVue,
+    InstaShaynaVue,
+    PartnerShaynaVue,
+    FooterShaynaVue
+}
+}
+</script>
